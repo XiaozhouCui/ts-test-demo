b1c983ab88a39b4ab08e282f657e9f97b6ddb3d6
Joe Cui <xiaozhou.cui@gmail.com>
2021-05-23 22:47:26 +1000

    Project setup

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils'
+
+describe('Utils test suite', () => {
+  test('first test', () => {
+    const abc = Utils.toUpperCase('abc')
+    console.log('tests work!!')
+  })
+})
