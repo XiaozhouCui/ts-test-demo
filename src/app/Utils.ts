63429fce78720cedb9462c4966570e689a1ac290
Joe Cui <xiaozhou.cui@gmail.com>
2021-05-24 21:56:28 +1000

    Adding assertions

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,11 @@
+import { UrlWithParsedQuery, parse } from 'url'
+
 export class Utils {
-  public static toUpperCase(arg: string) {
-    return ''
+  public static parseUrl(url: string): UrlWithParsedQuery {
+    return parse(url, true)
+  }
+
+  public static toUpperCase(arg: string): string {
+    return arg.toUpperCase()
   }
 }
