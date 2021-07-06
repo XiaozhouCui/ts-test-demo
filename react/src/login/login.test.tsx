bd96994c7f6d51d70fbd89d7f60a43ae21fd9c27
Joe Cui <xiaozhou.cui@gmail.com>
2021-07-06 23:10:58 +1000

    Test user interaction: fireEvent, change, click

--- react/src/login/login.test.tsx
+++ react/src/login/login.test.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { act } from 'react-dom/test-utils'
 import { Login } from './login'
+import { fireEvent } from '@testing-library/react'
+import { LoginService } from '../services/LoginService'
 
 describe('Login component tests', () => {
   let container: HTMLDivElement
+  // don't call real http request in unit test
+  const loginServiceSpy = jest.spyOn(LoginService.prototype, 'login') // replace login method
+
   beforeEach(() => {
     // create a div container in the body
     container = document.createElement('div')
@@ -31,8 +36,29 @@ describe('Login component tests', () => {
 
   // make use of the attribute "data-test" in Login form
   it('Renders correctly initial document with data-test query', () => {
-    expect(container.querySelector('[data-test="login-form"]')).toBeInTheDocument()
-    expect(container.querySelector('[data-test="login-input"]')?.getAttribute('name')).toBe('login')
-    expect(container.querySelector('[data-test="password-input"]')?.getAttribute('name')).toBe('password')
+    expect(
+      container.querySelector('[data-test="login-form"]')
+    ).toBeInTheDocument()
+    expect(
+      container.querySelector('[data-test="login-input"]')?.getAttribute('name')
+    ).toBe('login')
+    expect(
+      container
+        .querySelector('[data-test="password-input"]')
+        ?.getAttribute('name')
+    ).toBe('password')
+  })
+
+  // test user interaction: fireEvent, change, click
+  it('Passes credentials correctly', () => {
+    const inputs = container.querySelectorAll('input')
+    const loginInput = inputs[0]
+    const passwordInput = inputs[1]
+    const loginButton = inputs[2]
+
+    fireEvent.change(loginInput, { target: { value: 'someUser' } })
+    fireEvent.change(passwordInput, { target: { value: 'somePassword' } })
+    fireEvent.click(loginButton)
+    expect(loginServiceSpy).toBeCalledWith('someUser', 'somePassword')
   })
 })
